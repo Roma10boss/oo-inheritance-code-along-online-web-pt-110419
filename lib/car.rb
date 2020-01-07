d9233511ba315808538f3144a97a6095af79fac9
Romario Gustave <romagustave10@gmail.com>
2020-01-07 23:56:47 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,11 +1,9 @@
 require_relative "./vehicle.rb"
 
 class Car 
-  attr_reader :wheel_size, :number
   
-  def initialize(wheel_size, number)
-    @wheel_size = wheel_size
-    @number = number
+  def go 
+    "vroo"
   end 
   
     
